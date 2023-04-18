49be4418893bbc98034fa9acb3e004c6a1f876a2
mattmagnell <mattmagnell@gmail.com>
2023-04-18 21:03:01 +0100

    0.0.6
    Basic Enemy AI implemented on Slime enemy.

--- The Tower/Scenes/enemies/Slime1.gd
+++ The Tower/Scenes/enemies/Slime1.gd
@@ -1,7 +1,68 @@
-extends Node2D
+extends CharacterBody2D
 
+enum EnemyState {
+	IDLE,
+	PATROL,
+	CHASE,
+	ATTACK
+}
 
+var state = EnemyState.IDLE
+@onready var player = get_tree().get_nodes_in_group("Player")[0] if get_tree().get_nodes_in_group("Player").size() > 0 else null
 
-func _ready():
-	pass
+const SPEED = 100
+const ACCELERATION = 10
+const PATROL_DISTANCE = 200
+const CHASE_DISTANCE = 300
+const ATTACK_DISTANCE = 20
+const STOP_CHASE_DISTANCE = 200
+
+@onready var patrol_target = position + Vector2(PATROL_DISTANCE, 0)
+
+func _physics_process(delta):
+	if player != null:
+		match state:
+			EnemyState.IDLE:
+				idle_logic(delta)
+			EnemyState.PATROL:
+				patrol_logic(delta)
+			EnemyState.CHASE:
+				chase_logic(delta)
+			EnemyState.ATTACK:
+				attack_logic(delta)
+
+func idle_logic(delta):
+	var distance_to_player = position.distance_to(player.position)
+	if distance_to_player <= CHASE_DISTANCE:
+		state = EnemyState.CHASE
+
+func patrol_logic(delta):
+	var target_direction = (patrol_target - position).normalized()
+	velocity = velocity.lerp(target_direction * SPEED, ACCELERATION * delta)
+	move_and_slide()
+
+	if position.distance_to(patrol_target) < 10:
+		patrol_target = position + Vector2(randf_range(-PATROL_DISTANCE, PATROL_DISTANCE), 0)
+
+	var distance_to_player = position.distance_to(player.position)
+	if distance_to_player <= CHASE_DISTANCE:
+		state = EnemyState.CHASE
+
+func chase_logic(delta):
+	var target_direction = (player.position - position).normalized()
+	velocity = velocity.lerp(target_direction * SPEED, ACCELERATION * delta)
+	move_and_slide()
+
+	var distance_to_player = position.distance_to(player.position)
+	if distance_to_player <= ATTACK_DISTANCE:
+		state = EnemyState.ATTACK
+	elif distance_to_player > STOP_CHASE_DISTANCE:
+		state = EnemyState.PATROL
+
+func attack_logic(delta):
+	# Implement attack behavior
+	# (add your attack logic here)
+	var distance_to_player = position.distance_to(player.position)
+	if distance_to_player > ATTACK_DISTANCE:
+		state = EnemyState.CHASE
 
