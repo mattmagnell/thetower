7fc7c22bb7a46429bd02b08c0e60c00bec5e9007
kingveneer <123672041+kingveneer@users.noreply.github.com>
2023-05-31 14:03:21 -0400

    items showing up in world DONE

--- The Tower/ItemObject.gd
+++ The Tower/ItemObject.gd
@@ -1,8 +1,8 @@
 @tool
 extends Node2D
 
-@export var item_name : String = ""
-@export var description : String = ""
+@export var item_name : String 
+@export var description : String 
 @export var icon : Texture = null
 
 func use():
--- The Tower/Resources/newspells/Spell components/SpellScroll.gd
+++ The Tower/Resources/newspells/Spell components/SpellScroll.gd
@@ -1,24 +1,30 @@
 @tool
 extends "res://ItemObject.gd"
 
-@export var spell : Resource
+@export var spell : Resource:
+	get:
+		return _spell
+	set(value):
+		_spell = value
+		update_properties_from_spell()
+		
+var _spell : Resource
 	
-func _init():
+func _ready():
 	if spell != null:
 		update_properties_from_spell()
 
 # This method updates the name and texture properties based on the spell
 func update_properties_from_spell():
-	item_name = spell.name + " Scroll"
-	icon = spell.texture
-
-# This method is called when the script is reloaded, or when the spell property is changed
+	if _spell != null:
+		var sprite : Sprite2D = get_node("Area2D/ScrollSprite")
+		sprite.texture = _spell.texture
+		item_name = _spell.name + " Scroll"
+		icon = _spell.texture
+		
 func _get_configuration_warning():
-	if spell == null:
+	if _spell == null:
 		return "No spell resource set for this scroll."
 	else:
-		update_properties_from_spell()
 		return ""
-
-
 
